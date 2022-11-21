4c8a9bb776be3fe2499e8b55ce84a16f545d6ef7
Nikhil Gangeri <62614529+nik2611@users.noreply.github.com>
2022-11-22 00:33:40 +0530

    Time: 111 ms (68.96%), Space: 45.4 MB (5.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,21 +1,32 @@
 /**
- * Hash Map - 1 Pass
+ * Hash Map - 2 Pass
  * Time O(N) | Space O(N)
  * https://leetcode.com/problems/two-sum/
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = (nums, target, map = new Map()) => {
+var twoSum = (nums, target) => {
+    const map = getMap(nums);       /* Time O(N) | Space O(N) */
+
+    return getSum(nums, target, map)/* Time O(N) */
+}
+
+const getMap = (nums, map = new Map()) => {
     for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-        const num = nums[index];
-        const complement = (target - num);
+        map.set(nums[index], index);                        /* Space O(N) */
+    }
+
+    return map
+}
+
+const getSum = (nums, target, map) => {
+    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
+        const complement = target - nums[index];
         const sumIndex = map.get(complement);
 
-        const isTarget = map.has(complement)
-        if (isTarget) return [ index, sumIndex ];
-
-        map.set(num, index);                                /* Space O(N) */
+        const isTarget = map.has(complement) && (map.get(complement) !== index)
+        if (isTarget) return [ index, sumIndex ]
     }
 
     return [ -1, -1 ];
