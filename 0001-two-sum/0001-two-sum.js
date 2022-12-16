d8b9689897fa1c1dbe8c6ec6ad31480824062b75
Nikhil Gangeri <62614529+nik2611@users.noreply.github.com>
2022-12-16 21:26:18 +0530

    Time: 173 ms (32.98%), Space: 42.2 MB (78.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    var array = [];
+    for(let i=0; i<nums.length; i++) {
+        for(let j=i+1; j<=nums.length; j++) {
+            if(nums[i] + nums[j] == target){
+                array.push(i, j);
+                return array;
+            }
+        }
+    }
+    
+};
+
+twoSum([2,7,11,15], 9);
